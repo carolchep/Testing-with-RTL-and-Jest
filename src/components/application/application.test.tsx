c69487b8d368e0590491e9b03564b6385d70a25c
carolchep <carolchep@gmail.com>
2023-02-22 18:01:11 +0300

    getByRole-options

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -4,9 +4,11 @@ import {getByRole, render,screen } from "@testing-library/react"
 describe('Application',()=>{
     test('renders correctly',()=>{
         render(<Application/>)
-        const element= screen.getByRole('textbox')  
+        const element= screen.getByRole('textbox',{
+            name:'Name'
+        })  
         expect(element).toBeInTheDocument()
-        
+
         const jobLocation = screen.getByRole('combobox')
         expect(jobLocation).toBeInTheDocument()
 
@@ -15,6 +17,14 @@ describe('Application',()=>{
 
         const submitButton =screen.getByRole('button')
         expect(submitButton).toBeInTheDocument()
-
+        const BioElement =screen.getByRole('textbox',{
+            name:'Bio'
+        })
+        expect(BioElement).toBeInTheDocument()
+        const pageHeading =screen.getByRole('heading',{
+            level : 1
+        })
+       expect(pageHeading).toBeInTheDocument()
     })
 })
+//other getByRole options are name,level,hidden,selected,pressed
