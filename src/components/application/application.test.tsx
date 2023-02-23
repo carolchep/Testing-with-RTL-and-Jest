64fbc2ee1d351ab99082bda1cb4fc87cd43f1186
carolchep <carolchep@gmail.com>
2023-02-23 10:37:12 +0300

    getBYAltText

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -49,16 +49,21 @@ describe('getBylabelText', () => {
         const placeholder = screen.getByPlaceholderText("Fullname")
         expect(placeholder).toBeInTheDocument()
     })
-    test('getByText',()=>{
-        render(<Application/>)
+    test('getByText', () => {
+        render(<Application />)
         //getBytText can also take the select option as in getByPlaceHolderText
-        const paragraphElement=screen.getByText("All fields are mandatory")
+        const paragraphElement = screen.getByText("All fields are mandatory")
         expect(paragraphElement).toBeInTheDocument()
     })
-    test('getByDisplayValue',()=>{
-        render(<Application/>)
-        const name=screen.getByDisplayValue("Carol")
+    test('getByDisplayValue', () => {
+        render(<Application />)
+        const name = screen.getByDisplayValue("Carol")
         expect(name).toBeInTheDocument()
     })
-
+    test('getByAltText',()=>{
+        render(<Application/>)
+       // img,input area,customHtml
+        const ImageElement=screen.getByAltText("a person with a laptop")
+        expect(ImageElement).toBeInTheDocument()
+    })
 })
