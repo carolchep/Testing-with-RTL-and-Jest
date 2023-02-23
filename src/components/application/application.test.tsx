604e43dbd7abf1da1bbb33be5edeff9c3543e679
carolchep <carolchep@gmail.com>
2023-02-23 10:03:24 +0300

    get-by-label-Text

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -1,12 +1,12 @@
 import { Application } from "./application";
-import {getByRole, render,screen } from "@testing-library/react"
+import { getByRole, render, screen } from "@testing-library/react"
 
-describe('Application',()=>{
-    test('renders correctly',()=>{
-        render(<Application/>)
-        const element= screen.getByRole('textbox',{
-            name:'Name'
-        })  
+describe('Application', () => {
+    test('renders correctly', () => {
+        render(<Application />)
+        const element = screen.getByRole('textbox', {
+            name: 'Name'
+        })
         expect(element).toBeInTheDocument()
 
         const jobLocation = screen.getByRole('combobox')
@@ -15,16 +15,23 @@ describe('Application',()=>{
         const termsElement = screen.getByRole('checkbox')
         expect(termsElement).toBeInTheDocument()
 
-        const submitButton =screen.getByRole('button')
+        const submitButton = screen.getByRole('button')
         expect(submitButton).toBeInTheDocument()
-        const BioElement =screen.getByRole('textbox',{
-            name:'Bio'
+        const BioElement = screen.getByRole('textbox', {
+            name: 'Bio'
         })
         expect(BioElement).toBeInTheDocument()
-        const pageHeading =screen.getByRole('heading',{
-            level : 1
+        const pageHeading = screen.getByRole('heading', {
+            level: 1
         })
-       expect(pageHeading).toBeInTheDocument()
+        expect(pageHeading).toBeInTheDocument()
     })
 })
 //other getByRole options are name,level,hidden,selected,pressed
+describe('getBylabelText', () => {
+    test('should getLabelBytEext', () => {
+        render(<Application />)
+        const nameElement2 = screen.getByLabelText('Name')
+        expect(nameElement2).toBeInTheDocument()
+    })
+})
